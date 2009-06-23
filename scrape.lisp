7c98d5939de914aeffc703026bd52360fd6b3774
Ben Hyde <bhyde@pobox.com>
2009-06-23 13:58:11 -0400

    scrape.lisp

--- scrape.lisp
+++ scrape.lisp
@@ -18,7 +18,7 @@
 
 ;;; The actual scrapping
 
-(defun scrap-tracker-page (url)
+(defun scrape-tracker-page (url)
   "Pluck junk out of pages like http://www.ghostery.com/apps/chartbeat."
   (get-page url)
   (list
@@ -26,8 +26,8 @@
    (pluck "Website: <a rel=\"nofollow\" href=\"([^\"]*)\"")
    (pluck "<h1>([^<]*)</h1>")))
 
-(defun scrap-trackers-via-google ()
-  "Use google to find apps pages on ghostery.com, then scrap em."
+(defun scrape-trackers-via-google ()
+  "Use google to find apps pages on ghostery.com, then scrape em."
   (loop
     with u = "http://www.google.com/search?q=site:ghostery.com+%22Application+Owner%22&hl=en&sa=N"
     for i in '("0" "10" "20" "30" "40")
@@ -35,4 +35,4 @@
     do
  (cl-ppcre:do-register-groups (link)
      ("href=\"(http://www.ghostery.com/apps/[^\"]*)\"" p)
-   (format t "~&~{~4A ~40A ~A~}"  (scrap-tracker-page link)))))
+   (format t "~&~{~4A ~40A ~A~}"  (scrape-tracker-page link)))))
