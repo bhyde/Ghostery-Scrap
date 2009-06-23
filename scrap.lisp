e60802af8e6dd9edcc25a122040693e4bb6bcb18
Ben Hyde <bhyde@pobox.com>
2009-06-23 13:51:14 -0400

    Comments have no effect on the programs operation.

--- scrap.lisp
+++ scrap.lisp
@@ -1,5 +1,7 @@
 (package "COMMON-LISP-USER")
 
+;;; in the tradition of forth
+
 (defvar *pad*)
 
 (defun get-page (url &key parameters)
@@ -13,7 +15,19 @@
         (svref regs 0)
         nil)))
 
+
+;;; The actual scrapping
+
+(defun scrap-tracker-page (url)
+  "Pluck junk out of pages like http://www.ghostery.com/apps/chartbeat."
+  (get-page url)
+  (list
+   (pluck "found on.*<b>(\\d+)</b>")
+   (pluck "Website: <a rel=\"nofollow\" href=\"([^\"]*)\"")
+   (pluck "<h1>([^<]*)</h1>")))
+
 (defun scrap-trackers-via-google ()
+  "Use google to find apps pages on ghostery.com, then scrap em."
   (loop
     with u = "http://www.google.com/search?q=site:ghostery.com+%22Application+Owner%22&hl=en&sa=N"
     for i in '("0" "10" "20" "30" "40")
@@ -22,11 +36,3 @@
  (cl-ppcre:do-register-groups (link)
      ("href=\"(http://www.ghostery.com/apps/[^\"]*)\"" p)
    (format t "~&~{~4A ~40A ~A~}"  (scrap-tracker-page link)))))
-
-(defun scrap-tracker-page (url)
-  (get-page url)
-  (list
-   (pluck "found on.*<b>(\\d+)</b>")
-   (pluck "Website: <a rel=\"nofollow\" href=\"([^\"]*)\"")
-   (pluck "<h1>([^<]*)</h1>")))))
-
